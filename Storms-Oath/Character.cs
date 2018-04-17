ffb6fe8090b8c1fb21f7e1673e9f7db8849fe5f9
Izares <thebenpetrie@gmail.com>
2018-04-17 16:50:33 -0600

    Started Project[A

---
+++ Storms-Oath/Character.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StormsOath
+{
+    public class Character
+    {
+        
+
+        // Basic Constructer (No Params)
+        public Character()
+        {
+
+        }
+
+
+    }
+}
